9f76a80b6f6568458dec96eef38630191151fa8c
MirYarmuhammadtalpur <76510029+Yarmuhammadtalpur@users.noreply.github.com>
2025-07-23 15:29:08 +0500

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Admin API Failed 404");
+  res.send("Welcome to Admin API");
 });
 
 app.use("/admin", AdminRoutes);
