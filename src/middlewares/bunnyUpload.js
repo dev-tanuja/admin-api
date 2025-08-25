c25fc5115cf77c031baa1ddca89c0cc7432a30be
dev-tanuja <79660316+dev-tanuja@users.noreply.github.com>
2025-08-25 20:02:45 +0400

    Drop Down Api

--- src/middlewares/bunnyUpload.js
+++ src/middlewares/bunnyUpload.js
@@ -7,7 +7,7 @@ const upload = multer({
   limits: { fileSize: 10 * 1024 * 1024 },
   fileFilter: (req, file, cb) => {
     const ext = path.extname(file.originalname).toLowerCase()
-    if (['.jpg', '.jpeg', '.png', '.webp', '.mp4', 'mov'].includes(ext)) {
+    if (['.jpg', '.jpeg', '.png', '.webp', '.mp4', '.mov'].includes(ext)) {
       cb(null, true)
     } else {
       cb(new Error('Only image files are allowed'))
